2c088f2dcd469f03c88c3f1714ff69eb73433f6c
ncwalls <nate@makespaceweb.com>
2026-04-18 17:43:51 -0400

    animation

--- wp-content/themes/omythic-child/front-page.php
+++ wp-content/themes/omythic-child/front-page.php
@@ -56,7 +56,7 @@
 		<?php endif; ?>
 
 		<div class="hero-content">
-			<div class="container">
+			<div class="container scroll-animate-item">
 				<?php if($hero['title']): ?>
 					<h1 class="hero-title"><?php echo $hero['title']; ?></h1>
 				<?php endif; ?>
@@ -69,8 +69,15 @@
 				<?php if($hero['button']): $btn = $hero['button']; ?>
 					<a href="<?php echo $btn['url']; ?>" target="<?php echo esc_attr($btn['target']); ?>" class="button"><?php echo $btn['title']; ?></a>
 				<?php endif; ?>
+				<?php $contact_options = get_field('contact', 'option'); ?>
+				<?php if($contact_options && $contact_options['phone']): ?>
+					<a href="tel:<?php echo preg_replace('/[^0-9+]/', '', $contact_options['phone']); ?>" class="hero-phone">
+						<i class="fas fa-phone"></i>
+					</a>
+				<?php endif; ?>
 			</div>
 		</div>
+
 	</div>
 	<?php endif; ?>
 
@@ -82,7 +89,7 @@
 		<div class="container">
 
 			<?php if($intro): ?>
-			<div class="intro-left">
+			<div class="intro-left scroll-animate-item">
 				<?php if($intro['title']): ?>
 					<h2 class="section-title"><?php echo $intro['title']; ?></h2>
 				<?php endif; ?>
@@ -119,7 +126,7 @@
 			<?php endif; ?>
 
 			<?php if($intro_side && ($intro_side['title'] || $intro_side['content'])): ?>
-			<div class="intro-right">
+			<div class="intro-right scroll-animate-item">
 				<?php if($intro_side['title']): ?>
 					<h3 class="section-title"><?php echo $intro_side['title']; ?></h3>
 				<?php endif; ?>
@@ -142,7 +149,7 @@
 	<section class="home-section home-practice-areas">
 		<div class="container">
 			<?php if($pa_section['title'] || $pa_section['content']): ?>
-			<div class="section-header">
+			<div class="section-header scroll-animate-item">
 				<?php if($pa_section['title']): ?>
 					<h2 class="section-title"><?php echo $pa_section['title']; ?></h2>
 				<?php endif; ?>
@@ -152,7 +159,7 @@
 			</div>
 			<?php endif; ?>
 			<?php if($pa_section['practice_areas']): ?>
-			<ul class="practice-areas-grid">
+			<ul class="practice-areas-grid scroll-animate-item">
 				<?php foreach($pa_section['practice_areas'] as $pa_id): ?>
 					<?php
 						$pa_icon    = get_field('icon', $pa_id);
@@ -186,7 +193,7 @@
 		</div>
 		<?php if($pa_section['cta_title'] || $pa_section['cta_button']): ?>
 		<div class="section-cta">
-			<div class="container">
+			<div class="container scroll-animate-item">
 				<?php if($pa_section['cta_title']): ?>
 					<p class="section-cta-text"><?php echo $pa_section['cta_title']; ?></p>
 				<?php endif; ?>
@@ -205,7 +212,7 @@
 	<section class="home-section home-advantages">
 		<div class="container">
 			<?php if($adv_section['title'] || $adv_section['content']): ?>
-			<div class="section-header">
+			<div class="section-header scroll-animate-item">
 				<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="50" height="70" viewBox="0 0 50 70" fill="none">
 				  <path d="M43.1589 22.4354C40.8505 21.136 38.2243 20.3781 35.2617 20.1507V4.4286C35.2991 3.3783 35.6355 2.47958 36.2804 1.73246C36.9252 0.985336 37.7009 0.606361 38.6075 0.606361H39.2897V0H24.8318V0.606361H25.514C26.4206 0.606361 27.2056 0.985336 27.8692 1.73246C28.5327 2.47958 28.8692 3.3783 28.8692 4.4286V20.2156H10.4299V4.4286C10.4673 3.3783 10.8037 2.47958 11.4486 1.73246C12.0935 0.985336 12.8692 0.606361 13.7757 0.606361H14.4579V0H0V0.606361H0.682242C1.58878 0.606361 2.36448 0.985336 3.00934 1.73246C3.6542 2.47958 4 3.3783 4.02804 4.4286V38.06C3.99065 39.1103 3.6542 40.009 3.00934 40.7561C2.36448 41.5032 1.58878 41.8822 0.682242 41.8822H0V42.4886H14.4579V41.8822H13.7757C12.8692 41.8822 12.0935 41.5032 11.4486 40.7561C10.8037 40.009 10.4579 39.1103 10.4299 38.06V22.522H28.8692V35.9377C29.2804 36.1976 29.7103 36.4466 30.1776 36.7065C31.6916 37.5078 33.3365 38.309 35.1028 39.1103C35.1589 39.1319 35.2056 39.1536 35.2617 39.1861C35.3925 39.2402 35.5234 39.3052 35.6542 39.3593C36.1308 39.5759 36.5794 39.7816 37.028 39.9873C36.7664 39.8141 36.514 39.5975 36.2897 39.3268C35.6449 38.5797 35.2991 37.681 35.271 36.6307V23.2366C36.6355 23.4532 37.8224 23.9188 38.8037 24.6442C40.2056 25.6729 40.9159 27.1022 40.9159 28.9212C40.9159 30.7403 40.6542 30.762 40.1402 31.3575C39.6262 31.9422 39.028 32.3862 38.3645 32.6893C37.7009 32.9817 37.3645 33.1549 37.3645 33.2091C37.3645 33.653 37.6636 34.1295 38.2523 34.6492C38.8411 35.1689 39.7103 35.4505 40.8411 35.4938C42.7664 35.4938 44.3271 34.9199 45.5421 33.7613C46.7477 32.6027 47.3551 31.141 47.3551 29.376C47.3551 26.3334 45.9626 24.0162 43.1776 22.4462H43.1589V22.4354Z" fill="#262626"/>
 				  <path d="M47.8131 49.3426C46.5514 47.426 45 45.8776 43.1495 44.6974C42.0654 44.0044 40.757 43.2681 39.2243 42.4993C38.7383 42.2503 38.2336 42.0121 37.7103 41.7522C37.486 41.6439 37.2523 41.5357 37.0187 41.4274C36.5794 41.2217 36.1215 41.0051 35.6448 40.7994C35.514 40.7452 31.243 38.8612 28.4579 37.1287C26.0467 35.6236 24.8411 34.5517 24.1121 33.653C23.1775 32.4944 22.6635 31.5957 22.6635 29.874C22.6635 28.1524 22.9065 25.8677 25.2617 23.778C23.3925 24.1786 21.1682 25.283 19.7383 26.6365C18.299 28.0008 17.4766 29.6791 17.3832 32.8301C17.3084 35.4829 18.028 37.7026 19.3084 39.5C20.5888 41.2975 22.1402 42.7267 23.9719 43.8095C25.7944 44.8923 28.1869 46.0942 31.1495 47.4152C33.7103 48.5413 35.7383 49.5375 37.2149 50.4037C38.6916 51.2591 39.9346 52.3311 40.9533 53.6087C41.9626 54.8864 42.4673 56.4132 42.4673 58.1781C42.4673 59.943 42.0561 60.9717 41.243 62.271C40.4299 63.5704 39.2617 64.6207 37.729 65.4003C36.1962 66.1907 34.1121 66.5805 32.0934 66.5805C29.1308 66.5805 26.4953 65.422 25.0841 64.1659C23.6822 62.9099 23.1308 61.3074 23.1308 59.001C23.1308 56.6947 23.3271 56.9762 23.7196 56.3915C24.1121 55.8068 24.5607 55.3629 25.0467 55.0597C25.542 54.7673 25.785 54.5941 25.785 54.5399C25.785 54.096 25.486 53.6196 24.8972 53.0998C24.3084 52.5801 23.4392 52.2986 22.3084 52.2553C20.3832 52.2553 18.8224 52.8291 17.6075 53.9877C16.4018 55.1463 15.9532 56.7813 15.9532 58.5462C15.9532 62.0328 17.4766 65.0321 20.2149 66.8296C22.9532 68.627 26.9065 69.5149 32.0841 69.5149C37.2617 69.5149 38.4486 68.9518 41.0841 67.8149C43.7196 66.6888 45.8131 65.1296 47.3738 63.1373C48.9252 61.1449 49.7009 58.9252 49.7009 56.4673C49.7009 54.0094 49.0747 51.2374 47.8131 49.3209V49.3426Z" fill="#262626"/>
@@ -219,7 +226,7 @@
 			</div>
 			<?php endif; ?>
 			<?php if($adv_section['advantages']): ?>
-			<ul class="advantages-grid">
+			<ul class="advantages-grid scroll-animate-item">
 				<?php foreach($adv_section['advantages'] as $adv): ?>
 					<li class="advantage-card">
 						<?php if($adv['icon']): ?>
@@ -245,7 +252,7 @@
 	<section class="home-section home-staff">
 		<div class="container">
 			<?php if($staff_section['title'] || $staff_section['content']): ?>
-			<div class="section-header">
+			<div class="section-header scroll-animate-item">
 				<?php if($staff_section['title']): ?>
 					<h2 class="section-title"><?php echo $staff_section['title']; ?></h2>
 				<?php endif; ?>
@@ -257,7 +264,7 @@
 			<?php if($staff_section['staff']): ?>
 			<div class="staff-grid">
 				<div class="shadow"></div>
-				<div class="staff-grid-inner">
+				<div class="staff-grid-inner scroll-animate-item">
 					<?php foreach($staff_section['staff'] as $member_row): ?>
 						<?php
 							$member_id = $member_row['staff_member'];
@@ -302,7 +309,7 @@
 	<?php // ====== CTA 1 ====== ?>
 	<?php if($cta1 = get_field('cta_1')): if($cta1['title'] || $cta1['content']): ?>
 	<section class="home-section home-cta home-cta-1">
-		<div class="container">
+		<div class="container scroll-animate-item">
 			<?php if($cta1['title']): ?>
 				<h2 class="section-title"><?php echo $cta1['title']; ?></h2>
 			<?php endif; ?>
@@ -322,7 +329,7 @@
 	<section class="home-section home-results">
 		<div class="container">
 			<?php if($results_section['title'] || $results_section['content']): ?>
-			<div class="section-header">
+			<div class="section-header scroll-animate-item">
 				<?php if($results_section['title']): ?>
 					<h2 class="section-title"><?php echo $results_section['title']; ?></h2>
 				<?php endif; ?>
@@ -332,7 +339,7 @@
 			</div>
 			<?php endif; ?>
 			<?php if($results_section['results']): ?>
-			<ul class="results-grid">
+			<ul class="results-grid scroll-animate-item">
 				<?php foreach($results_section['results'] as $result_row): ?>
 					<?php
 						$case_id = $result_row['case_result'];
@@ -391,10 +398,10 @@
 			</ul>
 			<?php endif; ?>
 			<?php if($results_section['note']): ?>
-				<p class="results-note"><?php echo $results_section['note']; ?></p>
+				<p class="results-note scroll-animate-item"><?php echo $results_section['note']; ?></p>
 			<?php endif; ?>
 			<?php if($results_section['buttons']): ?>
-				<div class="section-buttons">
+				<div class="section-buttons scroll-animate-item">
 					<?php foreach($results_section['buttons'] as $btn_row): if($btn_row['button']): $btn = $btn_row['button']; ?>
 						<a href="<?php echo $btn['url']; ?>" target="<?php echo esc_attr($btn['target']); ?>" class="button"><?php echo $btn['title']; ?></a>
 					<?php endif; endforeach; ?>
@@ -410,7 +417,7 @@
 	<section class="home-section home-reviews">
 		<div class="container">
 			<?php if($reviews_section['title'] || $reviews_section['content']): ?>
-			<div class="section-header">
+			<div class="section-header scroll-animate-item">
 				<?php if($reviews_section['title']): ?>
 					<h2 class="section-title"><?php echo $reviews_section['title']; ?></h2>
 				<?php endif; ?>
@@ -434,7 +441,7 @@
 			</div>
 			<?php endif; ?>
 			<?php if($reviews_section['reviews']): ?>
-			<div class="reviews-slider-wrap">
+			<div class="reviews-slider-wrap scroll-animate-item">
 				<div class="reviews-slider">
 					<?php foreach($reviews_section['reviews'] as $review_row): ?>
 						<?php
@@ -485,7 +492,7 @@
 	<section class="home-section home-faq">
 		<div class="container">
 			<?php if($faq_section['title'] || $faq_section['content']): ?>
-			<div class="section-header">
+			<div class="section-header scroll-animate-item">
 				<?php if($faq_section['title']): ?>
 					<h2 class="section-title"><?php echo $faq_section['title']; ?></h2>
 				<?php endif; ?>
@@ -495,7 +502,7 @@
 			</div>
 			<?php endif; ?>
 			<?php if($faq_section['faqs']): ?>
-			<ul class="faq-list">
+			<ul class="faq-list scroll-animate-item">
 				<?php foreach($faq_section['faqs'] as $faq_id): ?>
 					<?php
 						$faq_question = get_the_title($faq_id);
@@ -533,7 +540,7 @@
 	<?php // ====== CTA 2 ====== ?>
 	<?php if($cta2 = get_field('cta_2')): if($cta2['title'] || $cta2['content']): ?>
 	<section class="home-section home-cta home-cta-2">
-		<div class="container">
+		<div class="container scroll-animate-item">
 			<?php if($cta2['title']): ?>
 				<h2 class="section-title"><?php echo $cta2['title']; ?></h2>
 			<?php endif; ?>
@@ -556,7 +563,7 @@
 		  <path d="M43.1589 22.4354C40.8505 21.136 38.2243 20.3781 35.2617 20.1507V4.4286C35.2991 3.3783 35.6355 2.47958 36.2804 1.73246C36.9252 0.985336 37.7009 0.606361 38.6075 0.606361H39.2897V0H24.8318V0.606361H25.514C26.4206 0.606361 27.2056 0.985336 27.8692 1.73246C28.5327 2.47958 28.8692 3.3783 28.8692 4.4286V20.2156H10.4299V4.4286C10.4673 3.3783 10.8037 2.47958 11.4486 1.73246C12.0935 0.985336 12.8692 0.606361 13.7757 0.606361H14.4579V0H0V0.606361H0.682242C1.58878 0.606361 2.36448 0.985336 3.00934 1.73246C3.6542 2.47958 4 3.3783 4.02804 4.4286V38.06C3.99065 39.1103 3.6542 40.009 3.00934 40.7561C2.36448 41.5032 1.58878 41.8822 0.682242 41.8822H0V42.4886H14.4579V41.8822H13.7757C12.8692 41.8822 12.0935 41.5032 11.4486 40.7561C10.8037 40.009 10.4579 39.1103 10.4299 38.06V22.522H28.8692V35.9377C29.2804 36.1976 29.7103 36.4466 30.1776 36.7065C31.6916 37.5078 33.3365 38.309 35.1028 39.1103C35.1589 39.1319 35.2056 39.1536 35.2617 39.1861C35.3925 39.2402 35.5234 39.3052 35.6542 39.3593C36.1308 39.5759 36.5794 39.7816 37.028 39.9873C36.7664 39.8141 36.514 39.5975 36.2897 39.3268C35.6449 38.5797 35.2991 37.681 35.271 36.6307V23.2366C36.6355 23.4532 37.8224 23.9188 38.8037 24.6442C40.2056 25.6729 40.9159 27.1022 40.9159 28.9212C40.9159 30.7403 40.6542 30.762 40.1402 31.3575C39.6262 31.9422 39.028 32.3862 38.3645 32.6893C37.7009 32.9817 37.3645 33.1549 37.3645 33.2091C37.3645 33.653 37.6636 34.1295 38.2523 34.6492C38.8411 35.1689 39.7103 35.4505 40.8411 35.4938C42.7664 35.4938 44.3271 34.9199 45.5421 33.7613C46.7477 32.6027 47.3551 31.141 47.3551 29.376C47.3551 26.3334 45.9626 24.0162 43.1776 22.4462H43.1589V22.4354Z" fill="#9B7839"/>
 		  <path d="M47.8131 49.3426C46.5514 47.426 45 45.8776 43.1495 44.6974C42.0654 44.0044 40.757 43.2681 39.2243 42.4993C38.7383 42.2503 38.2336 42.0121 37.7103 41.7522C37.486 41.6439 37.2523 41.5357 37.0187 41.4274C36.5794 41.2217 36.1215 41.0051 35.6448 40.7994C35.514 40.7452 31.243 38.8612 28.4579 37.1287C26.0467 35.6236 24.8411 34.5517 24.1121 33.653C23.1775 32.4944 22.6635 31.5957 22.6635 29.874C22.6635 28.1524 22.9065 25.8677 25.2617 23.778C23.3925 24.1786 21.1682 25.283 19.7383 26.6365C18.299 28.0008 17.4766 29.6791 17.3832 32.8301C17.3084 35.4829 18.028 37.7026 19.3084 39.5C20.5888 41.2975 22.1402 42.7267 23.9719 43.8095C25.7944 44.8923 28.1869 46.0942 31.1495 47.4152C33.7103 48.5413 35.7383 49.5375 37.2149 50.4037C38.6916 51.2591 39.9346 52.3311 40.9533 53.6087C41.9626 54.8864 42.4673 56.4132 42.4673 58.1781C42.4673 59.943 42.0561 60.9717 41.243 62.271C40.4299 63.5704 39.2617 64.6207 37.729 65.4003C36.1962 66.1907 34.1121 66.5805 32.0934 66.5805C29.1308 66.5805 26.4953 65.422 25.0841 64.1659C23.6822 62.9099 23.1308 61.3074 23.1308 59.001C23.1308 56.6947 23.3271 56.9762 23.7196 56.3915C24.1121 55.8068 24.5607 55.3629 25.0467 55.0597C25.542 54.7673 25.785 54.5941 25.785 54.5399C25.785 54.096 25.486 53.6196 24.8972 53.0998C24.3084 52.5801 23.4392 52.2986 22.3084 52.2553C20.3832 52.2553 18.8224 52.8291 17.6075 53.9877C16.4018 55.1463 15.9532 56.7813 15.9532 58.5462C15.9532 62.0328 17.4766 65.0321 20.2149 66.8296C22.9532 68.627 26.9065 69.5149 32.0841 69.5149C37.2617 69.5149 38.4486 68.9518 41.0841 67.8149C43.7196 66.6888 45.8131 65.1296 47.3738 63.1373C48.9252 61.1449 49.7009 58.9252 49.7009 56.4673C49.7009 54.0094 49.0747 51.2374 47.8131 49.3209V49.3426Z" fill="#9B7839"/>
 		</svg>
-		<div class="container">
+		<div class="container scroll-animate-item">
 			<div class="cta-3-left">
 				<?php if($cta3['icon']): ?>
 					<div class="cta-3-icon"><?php echo $cta3['icon']; ?></div>
